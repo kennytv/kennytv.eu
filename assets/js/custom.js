b20a1b8a93b408635b7ed90682d1bbeb633ef1da
Nassim Jahnke <nassim@njahnke.dev>
2024-05-17 18:05:12 +0200

    AAAAAAAAAAAAAAAAAAAAA

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,18 +1,15 @@
-
-//
-$(document).ready(function() {
-			$('#fullpage').fullpage({
-				'verticalCentered': false,
-				'scrollingSpeed': 600,
-				'autoScrolling': false,
-				'fitToSection': false,
-				'css3': true,
-				'navigation': false,
-			});
-		});
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        'verticalCentered': false,
+        'scrollingSpeed': 600,
+        'autoScrolling': false,
+        'fitToSection': false,
+        'css3': true,
+        'navigation': false,
+    });
+});
 
 // wow
-$(function()
-{
+$(function () {
     new WOW().init();
 })
